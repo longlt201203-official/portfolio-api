98a66d053dc4dd6b7ea035549f3c1d7823ab1c66
Le Thanh Long <longlt201203@gmail.com>
2025-05-08 17:29:37 +0700

    fix: fix db hostname

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { Env } from "@utils";
 import mongoose from "mongoose";
 
 export async function initDbConnection() {
-	return await mongoose.connect(`mongodb://localhost:${Env.DB_PORT}`, {
+	return await mongoose.connect(`mongodb://${Env.DB_HOST}:${Env.DB_PORT}`, {
 		user: Env.DB_USER,
 		pass: Env.DB_PASS,
 		dbName: Env.DB_NAME,
