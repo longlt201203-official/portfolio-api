f2c4934307f6f7ad95b4441aa7b2f61e926687fe
Le Thanh Long <longlt201203@gmail.com>
2025-05-09 14:07:33 +0700

    feat: update s3

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -5,6 +5,14 @@ export const s3Client = new S3Client({
 	endpoint: Env.S3_ENDPOINT,
 	region: Env.S3_REGION,
 	forcePathStyle: true,
+	...(Env.AWS_ACCESS_KEY_ID && Env.AWS_SECRET_ACCESS_KEY
+		? {
+				credentials: {
+					accessKeyId: Env.AWS_ACCESS_KEY_ID,
+					secretAccessKey: Env.AWS_SECRET_ACCESS_KEY,
+				},
+			}
+		: {}),
 });
 
 export function getS3Filepath(key: string) {
