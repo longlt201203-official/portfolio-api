1514a344c1b1fad3c3c642b91efacd49bbcbfd4f
Le Thanh Long <longlt201203@gmail.com>
2025-06-29 11:16:53 +0700

    fix: fix login api

--- src/modules/auth/dto/login.query.ts
+++ src/modules/auth/dto/login.query.ts
@@ -1,10 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { LoginTypeEnum } from "../enums";
+import { IsString } from "class-validator";
 
 export class LoginQuery {
 	@ApiProperty()
+	@IsString()
 	code: string;
 
 	@ApiProperty({ enum: LoginTypeEnum })
+	@IsString()
 	type: string;
 }
